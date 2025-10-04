62b5c34ce0185bb83526f755dbb82fb7f2385650
Henri Asseily <henri@asseily.com>
2025-10-05 00:44:44 +0300

    Reverted fullscreen change in MacOS. If window is maximized, going SDL_WINDOW_FULLSCREEN crashes/

--- source/frontends/sdl/sdlframe.cpp
+++ source/frontends/sdl/sdlframe.cpp
@@ -447,11 +447,7 @@ namespace sa2
                 else if (modifiers == KMOD_NONE)
                 {
                     myFullscreen = !myFullscreen;
-#if defined(__APPLE__)
-					SDL_SetWindowFullscreen(myWindow.get(), myFullscreen ? SDL_WINDOW_FULLSCREEN : 0);
-#else
 					SDL_SetWindowFullscreen(myWindow.get(), myFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
-#endif
                 }
                 break;
             }
